f0635711246ce55c553b03e4b8c9ebf907535a1f
AseelArafeh <arafehaseel@gmail.com>
2019-10-11 20:32:13 +0300

    Set an ID for member information tags

--- js/index.js
+++ js/index.js
@@ -1,26 +1,57 @@
-// Get the modal
-let modal = document.getElementById("myModal");
+class member {
+	constructor(name, email, major, role, biography) {
+		this.name = name;
+        this.email = email;
+        this.major = major;
+        this.role = role;
+        this.biography = biography;
+	}
+}
 
-// Get the button that opens the modal
-let btn = document.getElementById("myBtn");
+let teamMembers = [];
 
-// Get the <span> element that closes the modal
-let span = document.getElementsByClassName("close")[0];
+
+function saveButton(){
+
+    let name = document.getElementById('name').value;
+    let email = document.getElementById('email').value;
+    let allMajors = document.getElementById('all-majors');
+    let major = allMajors.options[allMajors.selectedIndex].text;
+    let allRoles = document.getElementById('all-roles');
+    let role = allRoles.options[allRoles.selectedIndex].text;
+    let biography = document.getElementById('biography');
+
+    let newMember = new member(name, email, major, role, biography);
+    teamMembers.push(newMember);
+    console.log(newMember);
+    console.log(teamMembers);
+    // add to localstorage 
+
+}
+
+
+
+
+
+// POP-UP
+let modal = document.getElementById("myModal");// Get the modal
+let btn = document.getElementById("myBtn");// Get the button that opens the modal
+let span = document.getElementsByClassName("close")[0];// Get the <span> element that closes the modal
 
 // When the user clicks the button, open the modal 
 btn.onclick = function() {
-modal.style.display = "block";
+    modal.style.display = "block";
 }
 
 // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
-modal.style.display = "none";
+    modal.style.display = "none";
 }
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
-if (event.target == modal) {
-modal.style.display = "none";
-}
+    if (event.target == modal) {
+        modal.style.display = "none";
+    }
 }
 
