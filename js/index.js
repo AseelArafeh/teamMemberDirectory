c198536cc82a507becf88e4fa8e336a7ca59d52f
AseelArafeh <arafehaseel@gmail.com>
2019-10-14 14:11:11 +0300

    Code for Delete Button

--- js/index.js
+++ js/index.js
@@ -1,3 +1,6 @@
+let teamMembers = [];
+let allEmails = new Set();
+
 
 class member {
 	constructor(name, email, major, role, biography) {
@@ -6,25 +9,30 @@ class member {
         this.major = major;
         this.role = role;
         this.biography = biography;
+
 	}
 }
 
-var teamMembers = [];
+showAllMembers();
+updateNumberOfItems();
 
 function updateNumberOfItems(){
-    document.getElementById("number-of-items").innerHTML = teamMembers.length + 1;
+    document.getElementById("number-of-items").innerHTML = teamMembers.length;
 }
 
 function showAllMembers() {
-    let allMembers = localStorage.getItem('allMembers');
+    const allMembers = localStorage.getItem('allMembers');
     console.log(allMembers);
     console.log(teamMembers);
-    //teamMembers = allMembers ? JSON.parse(localStorage.getItem('allMembers')) : [];
+    //from  local storage to my array then output members 
+    teamMembers = JSON.parse(allMembers) || [];
     console.log(teamMembers);
     let i;
+    //clear the member list befor 
+    document.getElementById('list-of-members').innerHTML = null;
     for (i = 0; i < teamMembers.length; i++) {
         let currentMember = `<li class="list-element">
-                                <div class="btn" onClick="this.parentNode.parentNode.removeChild(this.parentNode);">
+                                <div class="btn" onClick="deleteMember(this);">
                                     <div class="delete-btn">
                                         <div class="inner-symbol">
                                         </div>
@@ -33,7 +41,7 @@ function showAllMembers() {
                                 <div class="member-information"  id="myBtn" onClick="showPopUp(this);">
                                     <h3>${teamMembers[i].name}</h3>
                                     <span>
-                                        <span> ${teamMembers[i].email} </span> / <span> ${teamMembers[i].major} </span> / <span> ${teamMembers[i].role} </span>
+                                                          <span>${teamMembers[i].email}</span> / <span>${teamMembers[i].major}</span> / <span>${teamMembers[i].role}</span>
                                     </span>
                                     <p>
                                         ${teamMembers[i].biography}
@@ -44,7 +52,7 @@ function showAllMembers() {
     }
 }
 
-function saveButton(){
+function saveButton() {
 
     let name = document.getElementById('name').value;
     let email = document.getElementById('email').value;
@@ -52,22 +60,58 @@ function saveButton(){
     let major = allMajors.options[allMajors.selectedIndex].text;
     let allRoles = document.getElementById('all-roles');
     let role = allRoles.options[allRoles.selectedIndex].text;
-    let biography = document.getElementById('biography');
-
+    let biography = document.getElementById('biography').value;
+    if(name=="" || email=="" || major=="Major" || role=="Role" || biography.length<500){
+        alert("some field was empty");
+        return;
+    }
+    
+    if (allEmails.has(email) == 1) {
+        alert("email was already exist");
+        return;
+    }
+    allEmails.add(email);
     let newMember = new member(name, email, major, role, biography);
     teamMembers.push(newMember);
     console.log(newMember);
     console.log(teamMembers);
     // add to localstorage 
     const jsonString = JSON.stringify(teamMembers);
-    localStorage.setItem('allMembers', teamMembers);
+    console.log(teamMembers);
+    
+    localStorage.setItem('allMembers', jsonString);
     showAllMembers();
     updateNumberOfItems();
 
 }
+console.log(teamMembers);
 
+function deleteMember(elementToBeDeleted) {
 
+    console.log(elementToBeDeleted);// email
 
+    let emailToBeDeleted = elementToBeDeleted.parentNode.children[1].children[1].children[0].innerHTML;
+    allEmails.delete(emailToBeDeleted);
+    for(let i=0; i<teamMembers.length;i++){
+        console.log(teamMembers[i].email);
+        console.log(emailToBeDeleted);
+        if(teamMembers[i].email == emailToBeDeleted){
+            teamMembers.splice(i, 1);
+            return;
+        }
+    }
+    //remove from array 
+    console.log(teamMembers);
+    // set the updated array to localstorage 
+    const jsonString = JSON.stringify(teamMembers);
+    console.log(teamMembers);
+    localStorage.setItem('allMembers', jsonString);
+
+    elementToBeDeleted.parentNode.parentNode.removeChild(elementToBeDeleted.parentNode);
+    updateNumberOfItems();
+    showAllMembers();
+    
+}
 
 
 
@@ -76,8 +120,9 @@ function saveButton(){
 
 // When the user clicks any member, members's own information will apper in a pop up modal 
 function showPopUp (currentMember) {
-    console.log(currentMember.children[2].innerHTML);
-    
+    console.log(currentMember);
+ //let elementToBeDeleted = new member(currentMember.children[0].innerHTML, currentMember.children[1].children[0].innerHTML, currentMember.children[1].children[1].innerHTML, currentMember.children[1].children[2].innerHTML, currentMember.children[2].innerHTML);
+    //console.log(elementToBeDeleted);
     document.getElementById('list-of-members').innerHTML = `<div id="myModal" class="modal">
                                                                 <div class="close-container">
                                                                     <span class="close" onClick="hidePopUp()">&times;</span>
@@ -94,21 +139,24 @@ function showPopUp (currentMember) {
                                                                             ${currentMember.children[2].innerHTML}
                                                                         </p>
                                                                         <span>
-                                                                            <button type="button" class="delete-btn">DELETE</button>
+                                                                            <button type="button" class="delete-btn" >DELETE</button>
                                                                             <button type="button" class="save-btn">SAVE</button>
                                                                             <button type="button" class="cancel-btn" onClick="hidePopUp()">CANCEL</button>
                                                                         </span>
                                                                     </div>
                                                                 </div>
                                                             </div>`;
-                                                                
-    
+   
+  |                                                              
+                                                            
 }
 
 
 function hidePopUp () {
     let modal = document.getElementById("myModal");// Get the modal
     modal.style.display = "none";
+    showAllMembers();
+    updateNumberOfItems();
 }
 
 // When the user clicks anywhere outside of the modal, close it
